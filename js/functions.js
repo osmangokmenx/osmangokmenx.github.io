4fa0d7bc18e031478841bc2ed96451ac9a81c92d
osmangokmenx <52015838+osmangokmenx@users.noreply.github.com>
2022-09-29 14:55:28 +0300

    .gitignore added

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,10 @@
 let history = [];
 let activeHistoryIndex = null;
-let typer = document.querySelector("#typer");
-let input = document.querySelector("#terminal-input");
-let output = document.querySelector("#terminal-output");
-let cursor = document.querySelector("#cursor");
-let commandTemplateItem = document.querySelector("#command-template");
+let typer = document.querySelector('#typer');
+let input = document.querySelector('#terminal-input');
+let output = document.querySelector('#terminal-output');
+let cursor = document.querySelector('#cursor');
+let commandTemplateItem = document.querySelector('#command-template');
 let commandTemplate = "osmangokmenx@github.io: <span class='dir'>~</span> >";
 
 output.innerHTML =
@@ -13,47 +13,47 @@ commandTemplateItem.innerHTML = commandTemplate;
 
 function newLine(command) {
   let content;
-  switch (command.replace(/^\s+|\s+$/gm, "")) {
-    case "about":
+  switch (command.replace(/^\s+|\s+$/gm, '')) {
+    case 'about':
       content = about;
       break;
-    case "cv":
+    case 'cv':
       content = cv;
       break;
-    case "social":
+    case 'social':
       content = social;
       break;
-    case "contact":
+    case 'contact':
       content = contact;
       break;
-    case "projects":
+    case 'projects':
       content = projects;
       break;
-    case "vim":
+    case 'vim':
       content = vim;
-      redirect("https://github.com/osmangokmenx/init.vim");
+      redirect('https://github.com/osmangokmenx/init.lua');
       break;
 
-    case "source":
+    case 'source':
       content = source;
-      redirect("https://github.com/osmangokmenx/osmangokmenx.github.io");
+      redirect('https://github.com/osmangokmenx/osmangokmenx.github.io');
       break;
-    case "help":
+    case 'help':
       content = help;
       break;
-    case "history":
+    case 'history':
       content = history;
       break;
-    case "clear":
-      output.innerHTML = "";
+    case 'clear':
+      output.innerHTML = '';
       return true;
     default:
       content = defaulCommand;
       break;
   }
 
-  if (typeof content == "object") {
-    let arrayToText = "";
+  if (typeof content == 'object') {
+    let arrayToText = '';
     content?.map((item) => {
       arrayToText = `${arrayToText} <div class="command-list"> ${item} </div>`;
     });
@@ -64,16 +64,16 @@ function newLine(command) {
 }
 
 function resetCursor() {
-  cursor.style.left = "0px";
+  cursor.style.left = '0px';
 }
 
 function moveIt(count, e) {
   e = e || window.event;
   var keycode = e.keyCode || e.which;
   if (keycode == 37 && parseInt(cursor.style.left) >= 0 - (count - 1) * 10) {
-    cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
+    cursor.style.left = parseInt(cursor.style.left) - 10 + 'px';
   } else if (keycode == 39 && parseInt(cursor.style.left) + 10 <= 0) {
-    cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+    cursor.style.left = parseInt(cursor.style.left) + 10 + 'px';
   }
 }
 
@@ -84,6 +84,6 @@ function setInputValue(value) {
 
 function redirect(url) {
   setTimeout(() => {
-    window.open(url, "_blank").focus();
+    window.open(url, '_blank').focus();
   }, 700);
 }
